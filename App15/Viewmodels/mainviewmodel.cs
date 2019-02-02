68c8fb0f4bf0af7c8d032713e9f7dc4446c56bce
SalmanMKC <32169182+salmanmkc@users.noreply.github.com>
2019-02-02 18:09:55 +0000

    ajusted the layout a bit

--- App15/Viewmodels/mainviewmodel.cs
+++ App15/Viewmodels/mainviewmodel.cs
@@ -1,20 +1,29 @@
-﻿using System;
+﻿using App15.Command;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace App15.Viewmodels
 {
-    class MainViewModel
+     class MainViewModel :INotifyPropertyChanged
     {
+        
         private string newTaskName;
         private ObservableCollection<TaskViewModel> tasks = new ObservableCollection<TaskViewModel>();
         public string NewTaskName
         {
             get { return newTaskName; }
-            set { newTaskName = value; }
+            set {
+                if (newTaskName != value)
+                {
+                    newTaskName = value;
+                    OnPropertyChanged(nameof(NewTaskName));
+                }
+            }
         }
 
         public ObservableCollection<TaskViewModel> Tasks
@@ -23,7 +32,17 @@ namespace App15.Viewmodels
             set { tasks = value; }
         }
 
-    internal class TaskViewModel
+        public AddTask CreateTask => new AddTask();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        internal class TaskViewModel
     {
             public string TaskName { get; set; }
             public bool IsComplete { get; set; }
